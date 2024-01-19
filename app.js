692e168cd5507e0dfe6736e4081609ae71b8f5c0
Megha Sharma <95690178+meghasharma2001@users.noreply.github.com>
2024-01-19 19:13:31 +0530

    Add files via upload
    done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 // const path = require('path');  
-const port = process.env.port || 3000 ; 
+const port = process.env.PORT || 3000 ; 
 const {engine} = require("express-handlebars")
 
 
-app.use(express.static("images"));
+
 app.use(express.static("public"));
 
 app.set ("view engine" , "hbs");
